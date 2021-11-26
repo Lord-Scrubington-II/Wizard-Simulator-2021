5b0838a3c1a734c3ed47bb620c5ab678b3034da5
Lord-Scrubington-II (Zane W.) <zane.z.wang@gmail.com>
2021-11-25 23:55:21 -0800

    Updated the Enemy AI state machine

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,6 +8,7 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chaseRange;
+    [SerializeField] float exitRange;
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = float.PositiveInfinity;
@@ -19,12 +21,51 @@ public class EnemyAI : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+
+        // update the distance to the target and route the state
         distanceToTarget = Vector3.Distance(target.position, gameObject.transform.position);
-        if (distanceToTarget <= chaseRange) {
-            navMeshAgent.isStopped = false;
-            navMeshAgent.SetDestination(target.position);
-        } else navMeshAgent.isStopped = true;
+
+        if (isProvoked) {
+
+            if (distanceToTarget > exitRange) {
+                isProvoked = false;
+
+            } else {
+                EngageTarget();
+            }
+
+        } else if (distanceToTarget <= chaseRange) {
+            isProvoked = true;
+
+        } else {
+            isProvoked = false;
+            Idle();
+        }
     }
+
+    private void Idle() {
+        navMeshAgent.isStopped = true;
+    }
+
+    private void EngageTarget() {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance) {
+            ChaseTarget();
+        } 
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance) {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget() {
+        navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget() {
+        Debug.Log(name + " is attacking " + target.name);
+    }
+
 
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
