eef93227798494ebbaf5f8f5d3ee38c1440921ac
Yuzi <60211694+UUUZ0428@users.noreply.github.com>
2023-02-25 14:23:14 -0800

    changed enemy chasing speed, added esc to quit game

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void ReloadGame() {
-        SceneManager.LoadScene(1);
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
         Time.timeScale = 1; // re-enable gaming
     }
 
-    public void QuitGame() {
+    public void QuitGame()
+    {
         Application.Quit();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("escape pressed");
+            Application.Quit();
+        }
     }
 }
